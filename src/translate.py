3a569924325002226dde5da21bdd4a2577e465c9
Jany Muong <me.roumuong@gmail.com>
2023-03-02 20:13:31 +0300

    Text in French

--- src/translate.py
+++ src/translate.py
@@ -1,10 +1,9 @@
 import boto3
 
 client = boto3.client('translate', region_name='us-east-1')
-text_data = 'je suis Jany Muong'
+text_data = 'Salut, je suis Robot Person'
 
 response = client.translate_text(Text=text_data, SourceLanguageCode='auto',
     TargetLanguageCode='en')
 
-print(text_data)
 print(response['TranslatedText'])

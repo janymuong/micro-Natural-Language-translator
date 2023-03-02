f701c91a172566a22a56d83a8a3b7917c487840d
Jany Muong <me.roumuong@gmail.com>
2023-03-02 19:02:12 +0300

    Fix syntax errors

--- src/cli-translate.py
+++ src/cli-translate.py
@@ -5,16 +5,17 @@ import click
 @click.option('--phrase', prompt='Put in a phrase you want to translate',
     help='CLI tool that translates text')
 
-def translate_txt(phrase)
+def translate_txt(phrase):
     '''
-    Translate text command line tool
+    Translate text as a command line tool
     '''
+
     client = boto3.client('translate', region_name='us-east-1')
     click.echo(click.style(f"Translate phrase: {phrase}", fg='red'))
     response_txt = client.translate_text(Text=phrase, SourceLanguageCode='auto',
         TargetLanguageCode='en')
     text = response_txt['TranslatedText']
     click.echo(click.style(f"{text}", fg='white', bg='blue'))
-    
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     translate_txt()

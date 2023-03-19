fe51c68aaec61351c293470de1f9cd1adfa74187
Jany Muong <me.roumuong@gmail.com>
2023-03-19 11:28:14 +0300

    descriptive docstring

--- src/cli-translate.py
+++ src/cli-translate.py
@@ -7,15 +7,16 @@ import click
 
 def translate_txt(phrase):
     '''
-    Translate text as a command line tool
+    Translate text in command line
+    Relies on Python package - Click
     '''
 
     client = boto3.client('translate', region_name='us-east-1')
-    click.echo(click.style(f"Translate phrase: {phrase}", fg='red'))
+    click.echo(click.style(f'Translate phrase: {phrase}', fg='red'))
     response_txt = client.translate_text(Text=phrase, SourceLanguageCode='auto',
         TargetLanguageCode='en')
     text = response_txt['TranslatedText']
-    click.echo(click.style(f"{text}", fg='white', bg='blue'))
+    click.echo(click.style(f'{text}', fg='white', bg='blue'))
 
 if __name__ == '__main__':
     translate_txt()
